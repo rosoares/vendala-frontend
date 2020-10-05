66ac18d383021c112c4f4957a54a94246d95a775
Rodrigo Soares <rodrigosoares8899@gmail.com>
2020-10-04 22:45:46 -0300

    Finish create page make this create product with color variation

--- src/pages/Products/Create/index.js
+++ src/pages/Products/Create/index.js
@@ -12,23 +12,36 @@ const CreateProduct = () => {
   const [firstStock, setFirstStock] = useState();
   const [availableStock, setAvailableStock] = useState();
   const [price, setPrice] = useState();
+  const [color, setColor] = useState();
   const [hasColorVariation, setHasColorVariation] = useState(false);
   const [loading, setLoading] = useState(false);
   const history = useHistory();
-  const [variations, setVariations] = useState({})
+  const [variations, setVariations] = useState([])
 
   const _createProduct = (event) => {
     event.preventDefault();
     setLoading(true)
+    let data = undefined;
 
-    let data = {
-      name : name,
-      description : description,
-      slug : slug,
-      first_stock : firstStock,
-      available_stock : availableStock,
-      price : price,
-      hasColorVariation : hasColorVariation
+    if (! hasColorVariation) {
+      data = {
+        name : name,
+        description : description,
+        slug : slug,
+        first_stock : firstStock,
+        available_stock : availableStock,
+        price : price,
+        hasColorVariation : hasColorVariation
+      }
+    }
+    else {
+      data = {
+        name : name,
+        description : description,
+        slug : slug,
+        hasColorVariation : hasColorVariation,
+        variations: variations
+      }
     }
 
     api.post('products', data, config)
@@ -46,9 +59,28 @@ const CreateProduct = () => {
       .catch(error => alert(error))
   }
 
+  const _addColorVariation = () => {
+    let newVariations = variations;
+
+    newVariations.push({
+      color_id: color,
+      first_stock: firstStock,
+      available_stock: availableStock,
+      price: price
+    });
+
+    setVariations(newVariations);
+    setColor(null);
+    setFirstStock(null);
+    setAvailableStock(null);
+    setPrice(null);
+  }
+
   useEffect(() => {
     _getColors();
   }, [])
+
+  console.log(variations);
 
   return (
     <div>
@@ -113,7 +145,68 @@ const CreateProduct = () => {
               />
             </>
         }
-
+        {
+          hasColorVariation &&
+          <>
+            <select onChange={(event) => setColor(event.target.value)}>
+              <option value="">Selecione a cor</option>
+              {
+                colors.map((color, index) => (
+                  <option key={index} value={color.id}>{color.name}</option>
+                ))
+              }
+            </select>
+            <br />
+            <input
+              type="text"
+              name="first_stock"
+              placeholder="Estoque inicial"
+              required
+              onChange={(event) => setFirstStock(event.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              name="available_stock"
+              placeholder="Estoque disponível"
+              required
+              onChange={(event) => setAvailableStock(event.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              name="price"
+              placeholder="Preço"
+              required
+              onChange={(event) => setPrice(event.target.value)}
+            />
+            <br />
+            <button type="button" onClick={_addColorVariation}>Adcionar Variação</button>
+            <br />
+            <table>
+              <thead>
+                <tr>
+                  <th>Cor</th>
+                  <th>Estoque inicial</th>
+                  <th>Estoque disponível</th>
+                  <th>Preço</th>
+                </tr>
+              </thead>
+              <tbody>
+              {
+                variations.map((variation, index) => (
+                  <tr key={index}>
+                    <td>{variation.color_id}</td>
+                    <td>{variation.first_stock}</td>
+                    <td>{variation.available_stock}</td>
+                    <td>{variation.price}</td>
+                  </tr>
+                ))
+              }
+              </tbody>
+            </table>
+          </>
+        }
         <br />
         {
           loading ?
