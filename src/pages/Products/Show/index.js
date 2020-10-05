eda60c64d58baf0d6a5ab5ead2d8187ff2270fa6
Rodrigo Soares <rodrigosoares8899@gmail.com>
2020-10-04 22:45:03 -0300

    Fix show data of color variation at 0 index only if has not color varation

--- src/pages/Products/Show/index.js
+++ src/pages/Products/Show/index.js
@@ -28,41 +28,46 @@ const ShowProduct = () => {
       {
         <div>
           {
-            ! loading &&
+            !loading &&
             <div>
               <h3>{product.name}</h3>
               <p>{product.description}</p>
-              <p>Estoque inicial: {product.color_variations[0].first_stock}</p>
-              <p>Estoque disponível: {product.color_variations[0].available_stock}</p>
-              <p>Preço: {product.color_variations[0].price}</p>
+              {
+                product.color_variations[0].color == null &&
+                <>
+                  <p>Estoque inicial: {product.color_variations[0].first_stock}</p>
+                  <p>Estoque disponível: {product.color_variations[0].available_stock}</p>
+                  <p>Preço: {product.color_variations[0].price}</p>
+                </>
+              }
               <br/>
               {
                 product.color_variations[0].color !== null &&
-                  <>
-                    <h4>Variações</h4>
-                    <table>
-                      <thead>
-                      <tr>
-                        <th>Cor</th>
-                        <th>Estoque inicial</th>
-                        <th>Estoque disponível</th>
-                        <th>Preço</th>
-                      </tr>
-                      </thead>
-                      <tbody>
-                      {
-                        product.color_variations.map((variation, index) => (
-                          <tr key={index}>
-                            <td>{variation.color.name}</td>
-                            <td>{variation.first_stock}</td>
-                            <td>{variation.available_stock}</td>
-                            <td>{variation.price}</td>
-                          </tr>
-                        ))
-                      }
-                      </tbody>
-                    </table>
-                  </>
+                <>
+                  <h4>Variações</h4>
+                  <table>
+                    <thead>
+                    <tr>
+                      <th>Cor</th>
+                      <th>Estoque inicial</th>
+                      <th>Estoque disponível</th>
+                      <th>Preço</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                      product.color_variations.map((variation, index) => (
+                        <tr key={index}>
+                          <td>{variation.color.name}</td>
+                          <td>{variation.first_stock}</td>
+                          <td>{variation.available_stock}</td>
+                          <td>{variation.price}</td>
+                        </tr>
+                      ))
+                    }
+                    </tbody>
+                  </table>
+                </>
               }
             </div>
           }
